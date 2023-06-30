3637f65bad3555dd47431d24237f219eb47d862e
Hüseyin ABACI <huseyin.abaci@hotmail.com>
2023-06-30 15:22:56 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React,{Component} from "react";
+
+class App extends Component {
+
+  render(){
+    return(
+      <div className="App">
+        <h1>My firs React app</h1>
+        <p>Welcome =)</p>
+      </div>
+    )
+  }
+
+
+}
+
+export default App;
