2400dd77a257afdbcb54ab0dbea608163a1dd353
Hüseyin ABACI <huseyin.abaci@hotmail.com>
2023-06-30 20:11:30 +0300

    Complete

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,15 +1,35 @@
-import React,{Component} from "react";
+import React from "react";
 
-class Ninjas extends Component{
-    render(){
-        return(
-            <div className="ninja">
-                <div>Name: Ryu</div>
-                <div>Age: 30</div>
-                <div>Belt: Black</div>
-            </div>
-        )
-    }
-}
+const Ninjas = ({ ninjas,deleteNinja }) => {
+  //   const ninjasList = ninjas.map((ninja, index) => {
+  //     if (ninja.age > 20) {
+  //       return (
+  //         <div className="ninja" key={index}>
+  //           <div>Name: {ninja.name}</div>
+  //           <div>Age: {ninja.age}</div>
+  //           <div>Belt: {ninja.belt}</div>
+  //           <br></br>
+  //         </div>
+  //       );
+  //     } else {
+  //       return null;
+  //     }
+  //   });
+  return (
+    <div className="ninja-list">
+      {ninjas.map((ninja) => {
+        return ninja.age > 20 ? (
+          <div className="ninja" key={ninja.id}>
+            <div>Name: {ninja.name}</div>
+            <div>Age: {ninja.age}</div>
+            <div>Belt: {ninja.belt}</div>
+            <button onClick={() => {deleteNinja(ninja.id)}}>Delete</button>
+            <br></br>
+          </div>
+        ) : null;
+      })}
+    </div>
+  );
+};
 
 export default Ninjas;
